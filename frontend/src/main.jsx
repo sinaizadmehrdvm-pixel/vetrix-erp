bb795e40d5e4eae8b727e858173d8cbebef9adf4
sinaizadmehrdvm-pixel <sinaizadmehrdvm@gmail.com>
2026-07-11 22:50:05 +0330

    feat: apply selected theme globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { LanguageProvider } from "./localization/LanguageContext";
+import { ThemeProvider } from "./theme/ThemeContext";
 import "./index.css";
 import "./styles/rtl.css";
 import "./styles/fonts.css";
@@ -10,9 +11,11 @@ import "./styles/fonts.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
+      <ThemeProvider>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
